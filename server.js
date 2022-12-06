c95f8760b10af46502fc73c853e6dbfecc68b30b
rocha <eduardoprudenciorocha@gmail.com>
2022-12-06 14:31:25 -0300

    up

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
 const { default: mongoose } = require('mongoose');
-const routes = require('./routes');
+const routes = require('./routes')
 
 const app = express();
 const port = 3001; //
